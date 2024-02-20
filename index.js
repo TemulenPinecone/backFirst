27e9dd59bc111dc0c2d104e23172ae2aa2e9cd07
Тэмүлэн Галсанпунцаг <23LP2607@G401-CC0119.local>
2024-02-20 12:29:23 +0800

    0220 last

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require(`express`);
-const { products, users } = require("./dummy.json");
+const { products, users, newUser } = require("./dummy.json");
+const fs = require(`fs`);
 // const products = data.products;
 // const users = data.users;
 const app = express();
-
-// console.log(products);
+const bodyParser = require(`body-parser`);
+app.use(bodyParser.json());
 
 app.get(`/products`, (request, response) => {
   response.type = `application/json`;
@@ -22,6 +23,71 @@ app.get(`/usernames`, (request, response) => {
     return element.name;
   });
   response.send({ filteredByName });
+});
+
+// app.post("/newUser", (req, res) => {
+//   // const newUser = req.body;
+//   // console.log(newUser);
+
+//   fs.appendFile("dummyTest.json", JSON.stringify(newUser), function (err) {
+//     if (err) throw err;
+//     console.log("Saved!");
+//   });
+
+//   res.status(200);
+//   res.send("User added successfully");
+// });
+
+// app.post("/addUser", (req, res) => {
+//   fs.readFile("dummy.json", (error, data) => {
+//     if (error) {
+//       console.log("Error in reading file");
+//     } else {
+//       const jsonFile = JSON.parse(data.toString());
+//       jsonFile.users.push(newUser);
+//       fs.writeFile("dummy.json", JSON.stringify(jsonFile), (err) => {
+//         if (err) {
+//           console.log(err);
+//           res.send("error happened");
+//         } else {
+//           console.log("success");
+//           res.send("User added successfully");
+//         }
+//       });
+//     }
+//   });
+//   res.status(200);
+//   res.send("User added successfully");
+// });
+
+// app.post("/add-user", (req, res) => {
+//   const addUser = req.body;
+//   console.log(addUser);
+
+//   fs.readFile("dummy.json", (error, data) => {
+//     if (error) {
+//       console.log("Error in reading file");
+//     } else {
+//       const jsonFile = JSON.parse(data.toString());
+//       jsonFile.users.push(newUser);
+//       fs.writeFile("dummy.json", JSON.stringify(jsonFile), (err) => {
+//         if (err) {
+//           console.log(err);
+//           res.send("error happened");
+//         } else {
+//           console.log("success");
+//           res.send("User added successfully");
+//         }
+//       });
+//     }
+//   });
+//   res.status(200);
+//   res.send("User added successfully");
+// });
+
+app.post("/test", (req, res) => {
+  const testUser = req.body;
+  console.log(`TEST USER                                        `, testUser);
 });
 
 app.listen(3001, () => {
